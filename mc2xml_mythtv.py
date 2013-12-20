438d31d15a3bcf013bad36e99c83b2ef71c61e35
Anthony Candido <anthony@candido.ca>
2013-12-20 00:47:42 -0500

    Add mc2xml_mythtv.py

---
+++ mc2xml_mythtv.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python  
+import xmltv
+import MySQLdb
+filename = '/PATH/tvguidesat.xml'
+db = MySQLdb.connect(host="localhost",user="USER",passwd="PASSWORD",db="mythconverg")
+cur = db.cursor() 
+channels = xmltv.read_channels(open(filename, 'r'))
+for channel in channels:
+        cur.execute("UPDATE channel SET xmltvid='" + channel['id'] + "' WHERE channum=" + channel['display-name'][1][0])
